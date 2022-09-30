3820a727935be3766fc2ae50ffbf42a2cb4f1480
carterbond <carterbondltd@gmail.com>
2022-09-29 20:28:07 -0400

    notflix

---
+++ src/components/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAQuKTWL9U4XB7MRbRgK1ZO_TGxDA7rmFA",
+    authDomain: "netflix-react-clone-36229.firebaseapp.com",
+    projectId: "netflix-react-clone-36229",
+    storageBucket: "netflix-react-clone-36229.appspot.com",
+    messagingSenderId: "1023867830705",
+    appId: "1:1023867830705:web:f982d79f0200fde09dbd90"
+  };
+  
